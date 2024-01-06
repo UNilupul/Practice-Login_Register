344d6ae32837b24e4906d0484f32acca75b40f33
UNilupul <udaranilupul086@gmail.com>
2024-01-06 23:33:31 +0530

    create login page

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -1,14 +1,71 @@
-import React from 'react'
+import React from 'react';
 import './login.css';
+import { Link } from 'react-router-dom';
+
+import video from '../../loginAssets/video.mp4';
+import logo from '../../loginAssets/logo.png';
+
+import { FaUserShield } from "react-icons/fa";
+import { BsFillShieldLockFill } from "react-icons/bs";
+import { AiOutlineSwapRight } from "react-icons/ai";
 
 const Login = () => {
     return (
         <div>
-            This is login Page
-            <br />
-            <a href='register'>To Login</a>
-            <br />
-            <a href='dashboard'>To Dashboard</a>
+            <div className='loginPage flex'>
+                <div className='container flex'>
+                    <div className='videoDiv'>
+                        <video src={video} autoPlay muted loop ></video>
+
+                        <div className='textDiv'>
+                            <h2 className='title'>Create and Sell Extrodinary Products</h2>
+                            <p>Adopt the peace of nature!</p>
+                        </div>
+
+                        <div className='footerDiv flex'>
+                            <span className='text'>Don't have an Account?</span>
+                            <Link to={'register'}>
+                                <button className='btn'>SignUp</button>
+                            </Link>
+                        </div>
+                    </div>
+
+                    <div className='formDiv flex'>
+                        <div className='headerDiv'>
+                            <img src={logo} alt='Logo Image' />
+                            <h3>Welcome Back!</h3>
+                        </div>
+
+                        <form className='form grid' action=''>
+                            <span className='showMessage'>Login status will go here</span>
+                            <div className='inputDiv'>
+                                <label htmlFor='username'>Username</label>
+                                <div className='input flex'>
+                                    <FaUserShield className='icon' />
+                                    <input type='text' id='username' placeholder='Enter Username' />
+                                </div>
+                            </div>
+                            <div className='inputDiv'>
+                                <label htmlFor='password'>Password</label>
+                                <div className='input flex'>
+                                    <BsFillShieldLockFill className='icon' />
+                                    <input type='password' id='password' placeholder='Enter Password' />
+                                </div>
+                            </div>
+
+                            <button type='submit' className='btn'>
+                                <span>Login</span>
+                                <AiOutlineSwapRight className='icon' />
+                            </button>
+
+                            <span className='forgotPassword'>
+                                Forgot your password
+                                <a href='#'>Click here</a>
+                            </span>
+                        </form>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
